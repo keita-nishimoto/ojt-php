28bd622efa07efa3c16de5fc8ee02a8235f48d72
keita-koga <keita.koga.work@gmail.com>
2017-12-25 23:54:51 +0900

    modify PHPDOCに誤りがあったので修正

--- tests/Functional/BaseTestCase.php
+++ tests/Functional/BaseTestCase.php
@@ -25,10 +25,13 @@ class BaseTestCase extends \PHPUnit_Framework_TestCase
     /**
      * Process the application given a request method and URI
      *
-     * @param string $requestMethod the request method (e.g. GET, POST, etc.)
-     * @param string $requestUri the request URI
-     * @param array|object|null $requestData the request data
-     * @return \Slim\Http\Response
+     * @param $requestMethod
+     * @param $requestUri
+     * @param null $requestData
+     * @return \Psr\Http\Message\ResponseInterface|Response
+     * @throws \Exception
+     * @throws \Slim\Exception\MethodNotAllowedException
+     * @throws \Slim\Exception\NotFoundException
      */
     public function runApp($requestMethod, $requestUri, $requestData = null)
     {
