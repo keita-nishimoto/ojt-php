ef04ff006b02369ba59cef6ca310fe4d9bdac575
keita-koga <keita.koga.work@gmail.com>
2018-01-26 00:20:46 +0900

    modify #2 リクエストオブジェクトを生成しコントローラークラスに渡すように修正

--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,7 @@ $indexController = new \App\Controllers\IndexController();
 
 $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $router) {
     $router->addRoute('GET', '/', '\\App\\Controllers\\IndexController::index');
+    $router->addRoute('GET', '/users/{userId}', '\\App\\Controllers\\IndexController::index');
 });
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
@@ -28,10 +29,12 @@ switch ($routeInfo[0]) {
         // 実行する関数名が入ってくる
         $handler = $routeInfo[1];
 
+        $request = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
+
         // pathパラメータが入ってくる事を確認
         $vars = $routeInfo[2];
 
-        echo $handler($vars);
+        echo $handler($request, $vars);
         break;
     default:
         break;
