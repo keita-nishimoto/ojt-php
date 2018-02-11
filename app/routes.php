058e0de76434c14e0774090f8b8a32dddec28aba
keita-koga <keita.koga.work@gmail.com>
2018-02-12 00:49:52 +0900

    modify #9 try catchを書いても事実上意味がない為、書かないように変更

--- app/routes.php
+++ app/routes.php
@@ -2,48 +2,45 @@
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
-try {
-    $container = new \Slim\Container();
-    $container['notFoundHandler'] = function (\Slim\Container $container) {
-        return function () use ($container) {
-            $view = new \App\Views\ErrorView();
+$container = new \Slim\Container();
+$container['notFoundHandler'] = function (\Slim\Container $container) {
+    return function () use ($container) {
+        $view = new \App\Views\ErrorView();
 
-            return $container['response']
-                ->withStatus(404)
-                ->withHeader('Content-Type', 'text/html')
-                ->write($view->getNotFoundHtml());
-        };
+        return $container['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write($view->getNotFoundHtml());
     };
+};
 
-    $container['notAllowedHandler'] = function (\Slim\Container $container) {
-        return function () use ($container) {
-            $view = new \App\Views\ErrorView();
+$container['notAllowedHandler'] = function (\Slim\Container $container) {
+    return function () use ($container) {
+        $view = new \App\Views\ErrorView();
 
-            return $container['response']
-                ->withStatus(405)
-                ->withHeader('Content-Type', 'text/html')
-                ->write($view->getNotAllowedHtml());
-        };
+        return $container['response']
+            ->withStatus(405)
+            ->withHeader('Content-Type', 'text/html')
+            ->write($view->getNotAllowedHtml());
     };
+};
 
-    $app = new \Slim\App($container);
-    $app->get('/', function (Request $request, Response $response, array $args) {
-        $indexController = new \App\Controllers\IndexController();
-        $indexController->showIndex($request, $response, $args);
+$app = new \Slim\App($container);
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $indexController = new \App\Controllers\IndexController();
+    $indexController->showIndex($request, $response, $args);
 
-        return $response;
-    });
+    $logger = new \App\lib\Logger();
+    $logger->debug($response);
 
-    $app->get('/users/{userId}', function (Request $request, Response $response, array $args) {
-        $userController = new \App\Controllers\UserController();
-        $userController->showUser($request, $response, $args);
+    return $response;
+});
 
-        return $response;
-    });
+$app->get('/users/{userId}', function (Request $request, Response $response, array $args) {
+    $userController = new \App\Controllers\UserController();
+    $userController->showUser($request, $response, $args);
 
-    $app->run();
-} catch (\Slim\Exception\MethodNotAllowedException $e) {
-    // TODO 405 Method Not Allowedのレスポンスを返す
-} catch (\Exception $e) {
-    // TODO ここに入る時は重大な障害が起きているのでシステムエラーのページを表示させる
-}
+    return $response;
+});
+
+$app->run();
