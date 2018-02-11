da2a83f155bc626dc3a4c6b211d3351b7cccb9a8
keita-koga <keita.koga.work@gmail.com>
2018-02-12 00:44:38 +0900

    add #9 デバッグログを出力するクラスを追加

---
+++ app/lib/Logger.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\lib;
+
+use Monolog\Handler\StreamHandler;
+
+class Logger
+{
+    /**
+     * @var \Monolog\Logger
+     */
+    private $logger;
+
+    /**
+     * Logger constructor.
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        $loggingPath = __DIR__ . '/../../logs/app.log';
+        $this->logger = new \Monolog\Logger('ojt-php');
+
+        $this->logger->pushHandler(
+            new StreamHandler($loggingPath, \Monolog\Logger::DEBUG)
+        );
+    }
+
+    /**
+     * デバッグログを出力する
+     *
+     * @param $var
+     */
+    public function debug($var)
+    {
+        $context = [
+            'debugValue' => $var,
+        ];
+
+        $this->logger->debug('App\lib\Logger:debug', $context);
+    }
+}
