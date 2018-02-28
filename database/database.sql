277b0f35b9dc424e89e91df4874fae0e079156a3
keita-koga <keita.koga.work@gmail.com>
2018-03-01 01:01:04 +0900

    modify #23 テーブルを正規化

--- database/database.sql
+++ database/database.sql
@@ -9,12 +9,24 @@ FLUSH PRIVILEGES;
 -- 仮ユーザー登録で利用するメインテーブル
 CREATE TABLE `preregisters` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `token` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `is_registered` tinyint(1) NOT NULL DEFAULT '0',
+  `lock_version` int(10) unsigned NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
+-- 仮ユーザー登録時に発行する認証トークンを管理するテーブル
+CREATE TABLE `preregisters_tokens` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `register_id` int(10) unsigned NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `expired_on` datetime NOT NULL,
   `lock_version` int(10) unsigned NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uq_preregisters_01` (`token`)
+  UNIQUE KEY `uq_preregisters_tokens_01` (`register_id`),
+  UNIQUE KEY `uq_preregisters_tokens_02` (`token`),
+  CONSTRAINT `fk_preregisters_tokens_01` FOREIGN KEY (`register_id`) REFERENCES `preregisters` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
