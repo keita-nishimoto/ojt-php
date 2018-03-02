4812d721b666296c36eb36cf9ce92fbaf3ceca16
keita-koga <keita.koga.work@gmail.com>
2018-03-03 02:56:15 +0900

    add #23 仮登録用のリポジトリクラスを仮実装し、テストケースを追加

---
+++ app/models/repository/PreregistrationRepository.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * PreregistrationRepository
+ */
+
+namespace App\Models\Repository;
+
+/**
+ * Class PreregistrationRepository
+ *
+ * @package App\Models\Repository
+ */
+class PreregistrationRepository
+{
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+
+    /**
+     * PreregistrationRepository constructor.
+     *
+     * @param \PDO $pdo
+     */
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * トークンを作成する
+     * TODO 仮実装
+     *
+     * @return bool
+     */
+    public function createToken()
+    {
+        $sql = 'INSERT INTO preregisters (is_registered) VALUES (0)';
+
+        $statement = $this->pdo->prepare($sql);
+
+        return $statement->execute();
+    }
+}
