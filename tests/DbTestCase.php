00edd1d2b552b78378bea6fd9bc69c4cbf9cb8d1
keita-koga <keita.koga.work@gmail.com>
2018-03-08 00:03:00 +0900

    modify #31 テスト用のPDO生成ロジックにATTR_ERRMODEとATTR_DEFAULT_FETCH_MODEを設定

--- tests/DbTestCase.php
+++ tests/DbTestCase.php
@@ -28,21 +28,9 @@ class DbTestCase extends TestCase
      */
     public function getConnection()
     {
-        $dotenv = new Dotenv(__DIR__ . '/../');
-        $dotenv->load();
+        $this->pdo = $this->createPdo();
 
-        $dbName = getenv('TEST_DB_NAME');
-        $dns = sprintf('mysql:dbname=%s;host=localhost', $dbName);
-
-        $pdo = new \PDO(
-            $dns,
-            getenv('TEST_DB_USER'),
-            getenv('TEST_DB_PASSWORD')
-        );
-
-        $this->pdo = $pdo;
-
-        return $this->createDefaultDBConnection($pdo);
+        return $this->createDefaultDBConnection($this->pdo);
     }
 
     /**
@@ -58,5 +46,33 @@ class DbTestCase extends TestCase
     protected function getPdo()
     {
         return $this->pdo;
+    }
+
+    /**
+     * テスト用のPDOオブジェクトを生成する
+     *
+     * @return \PDO
+     */
+    private function createPdo()
+    {
+        $dotenv = new Dotenv(__DIR__ . '/../');
+        $dotenv->load();
+
+        $dbName = getenv('TEST_DB_NAME');
+        $dns = sprintf('mysql:dbname=%s;host=localhost', $dbName);
+
+        $options = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        ];
+
+        $pdo = new \PDO(
+            $dns,
+            getenv('TEST_DB_USER'),
+            getenv('TEST_DB_PASSWORD'),
+            $options
+        );
+
+        return $pdo;
     }
 }
